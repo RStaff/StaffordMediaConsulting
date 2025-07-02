6f130afb5023678498951be21d5e80f46085bed7
Ross Stafford <rossstafford@mac.mynetworksettings.com>
2025-07-02 19:33:56 -0400

    refactor: extract financial_pipeline into reusable module

--- infra/modules/financial_pipeline/main.tf
+++ infra/modules/financial_pipeline/main.tf
@@ -1,18 +1,16 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_s3_bucket" "tfstate" {
-  bucket = "ross-data-pipeline-tfstate"
-//  acl    = "private"
-}
-
-resource "aws_dynamodb_table" "lock" {
-  name         = "ross-data-pipeline-tfstate-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
+terraform {
+  backend "s3" {
+    bucket         = "ross-data-pipeline-tfstate"
+    key            = "${env}/financial_pipeline/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "ross-data-pipeline-tfstate-locks"
   }
+}
+
+module "financial_pipeline" {
+  source         = "../../../infra/modules/financial_pipeline"
+  bucket_name    = "ross-data-pipeline-tfstate"
+  key            = "${env}/financial_pipeline/terraform.tfstate"
+  region         = "us-east-1"
+  dynamodb_table = "ross-data-pipeline-tfstate-locks"
 }
