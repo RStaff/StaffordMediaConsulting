561bd8d50e11c1442934df16eafeddaab339f428
Ross Stafford <rossstafford@mac.mynetworksettings.com>
2025-07-02 19:56:53 -0400

    refactor: remove backend block from financial_pipeline module

--- infra/modules/financial_pipeline/main.tf
+++ infra/modules/financial_pipeline/main.tf
@@ -1,11 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket         = "ross-data-pipeline-tfstate"
-    key            = "${env}/financial_pipeline/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "ross-data-pipeline-tfstate-locks"
-  }
-}
 
 module "financial_pipeline" {
   source         = "../../../infra/modules/financial_pipeline"
