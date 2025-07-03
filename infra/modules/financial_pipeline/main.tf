e5b1231d023f0270c17f12a58cc7d6fb6586a528
Ross Stafford <rossstafford@mac.mynetworksettings.com>
2025-07-02 21:31:58 -0400

    fix: remove backend block from financial_pipeline module

--- infra/modules/financial_pipeline/main.tf
+++ infra/modules/financial_pipeline/main.tf
@@ -1,8 +1,3 @@
-
-module "financial_pipeline" {
-  source         = "../../../infra/modules/financial_pipeline"
-  bucket_name    = "ross-data-pipeline-tfstate"
-  key            = "${env}/financial_pipeline/terraform.tfstate"
   region         = "us-east-1"
   dynamodb_table = "ross-data-pipeline-tfstate-locks"
 }
