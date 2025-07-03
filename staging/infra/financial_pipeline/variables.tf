9a5c2f7d7dd8d563779e18d44c8b51f373745a0e
Ross Stafford <rossstafford@mac.mynetworksettings.com>
2025-07-02 22:48:47 -0400

    fix: standardize per-env financial_pipeline stubs

--- staging/infra/financial_pipeline/variables.tf
+++ staging/infra/financial_pipeline/variables.tf
@@ -1,11 +1,9 @@
 variable "bucket_name" {
   default = "ross-data-pipeline-tfstate"
 }
-
 variable "region" {
   default = "us-east-1"
 }
-
 variable "dynamodb_table" {
   default = "ross-data-pipeline-tfstate-locks"
 }
